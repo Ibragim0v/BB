ac9f55e865bf00d564dcec573947a0f78a067323
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-05-21 14:10:34 +0500

    feat: bobur-blog finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,84 +2,99 @@ import "./Header.scss";
 import logo from "../../assets/images/logo.svg";
 import search from "../../assets/images/search.svg";
 import { NavLink } from "react-router-dom";
-import close from "../../assets/images/close.svg";
 import burger from "../../assets/images/burger.svg";
+import { useState } from "react";
 
 export function Header() {
+  const [burgerBtn, setBurgerBtn] = useState(false);
+
+  const handleBurger = () => {
+    if (burgerBtn === true) {
+      setBurgerBtn(false);
+    } else {
+      setBurgerBtn(true);
+    }
+  };
+
   return (
     <header className='header header__container'>
       <div className='header__wrapper'>
-        <div className='header__logo'>
-          <img
-            className='header__logo-img'
-            src={logo}
-            alt='logo'
-            width={210}
-            height={58}
-          />
+        <div className='header__mobile'>
+          <div className='header__logo'>
+            <img
+              className='header__logo-img'
+              src={logo}
+              alt='logo'
+              width={210}
+              height={58}
+            />
+          </div>
+          <div className='header__menu'>
+            <button onClick={handleBurger} className='header__burger'>
+              <img className='header__on' src={burger} alt='burger' />
+            </button>
+          </div>
         </div>
 
-        <ul className='header__list'>
-          <li className='header__item'>
-            <NavLink
-              to={"/"}
-              className={({ isActive }) =>
-                isActive
-                  ? "header__item-link header__item-link--active"
-                  : "header__item-link"
-              }>
-              All
-            </NavLink>
-          </li>
-
-          <li className='header__item'>
-            <NavLink
-              to={"/design"}
-              className={({ isActive }) =>
-                isActive
-                  ? "header__item-link header__item-link--active"
-                  : "header__item-link"
-              }>
-              Design Theory
-            </NavLink>
-          </li>
-
-          <li className='header__item'>
-            <NavLink
-              to={"/ux"}
-              className={({ isActive }) =>
-                isActive
-                  ? "header__item-link header__item-link--active"
-                  : "header__item-link"
-              }>
-              UX
-            </NavLink>
-          </li>
-
-          <li className='header__item'>
-            <NavLink
-              to={"/ui"}
-              className={({ isActive }) =>
-                isActive
-                  ? "header__item-link header__item-link--active"
-                  : "header__item-link"
-              }>
-              UI
-            </NavLink>
-          </li>
-
-          <li className='header__item'>
-            <NavLink
-              to={"/typography"}
-              className={({ isActive }) =>
-                isActive
-                  ? "header__item-link header__item-link--active"
-                  : "header__item-link"
-              }>
-              Typography
-            </NavLink>
-          </li>
-        </ul>
+        <nav className={`header__nav ${!burgerBtn && "d-none"}`}>
+          <ul className='header__list'>
+            <li className='header__item'>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__item-link header__item-link--active"
+                    : "header__item-link"
+                }>
+                All
+              </NavLink>
+            </li>
+            <li className='header__item'>
+              <NavLink
+                to={"/design"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__item-link header__item-link--active"
+                    : "header__item-link"
+                }>
+                Design Theory
+              </NavLink>
+            </li>
+            <li className='header__item'>
+              <NavLink
+                to={"/ux"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__item-link header__item-link--active"
+                    : "header__item-link"
+                }>
+                UX
+              </NavLink>
+            </li>
+            <li className='header__item'>
+              <NavLink
+                to={"/ui"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__item-link header__item-link--active"
+                    : "header__item-link"
+                }>
+                UI
+              </NavLink>
+            </li>
+            <li className='header__item'>
+              <NavLink
+                to={"/typography"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "header__item-link header__item-link--active"
+                    : "header__item-link"
+                }>
+                Typography
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
         <form className='header__form'>
           <input className='header__input' type='text' placeholder='search' />
@@ -93,13 +108,6 @@ export function Header() {
             />
           </button>
         </form>
-
-        <div className='header__menu'>
-          <button className='header__burger'>
-            <img className='header__on' src={close} alt='close' />
-            <img className='header__off' src={burger} alt='burger' />
-          </button>
-        </div>
       </div>
     </header>
   );
